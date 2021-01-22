263f0fcfd0ae0f84868d6c0a6a8ad3b4ba320ca1
Radu <radu.nicolae.botezatu@gmail.com>
2021-01-22 18:13:05 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,10 +11,17 @@ input.onGesture(Gesture.LogoDown, function () {
     sprite.change(LedSpriteProperty.Y, -1)
 })
 let sprite: game.LedSprite = null
+game.setLife(5)
 sprite = game.createSprite(2, 2)
 soundExpression.giggle.playUntilDone()
 basic.forever(function () {
     if (sprite.isTouchingEdge()) {
-        sprite.ifOnEdgeBounce()
+        game.removeLife(1)
+        soundExpression.sad.playUntilDone()
+        basic.pause(200)
+    }
+    if (sprite.get(LedSpriteProperty.X) == 3 && sprite.get(LedSpriteProperty.Y) == 1) {
+        game.addScore(1)
+        basic.pause(200)
     }
 })
