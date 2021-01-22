f8aca69e215e7205859e5c024108112b1d3bf065
Radu <radu.nicolae.botezatu@gmail.com>
2021-01-22 17:42:29 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+input.onGesture(Gesture.LogoUp, function () {
+    sprite.change(LedSpriteProperty.Y, 1)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    sprite.change(LedSpriteProperty.X, -1)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    sprite.change(LedSpriteProperty.X, 1)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    sprite.change(LedSpriteProperty.Y, -1)
+})
+let sprite: game.LedSprite = null
+sprite = game.createSprite(2, 2)
+soundExpression.giggle.playUntilDone()
+basic.forever(function () {
+    if (sprite.isTouchingEdge()) {
+        sprite.ifOnEdgeBounce()
+    }
+})
